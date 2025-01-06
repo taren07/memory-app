c4c21f1bae46065f05dcc3634d5f1e53229bb197
taren07 <taro.enchi@gmail.com>
2025-01-07 00:03:59 +0900

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,14 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+	{ files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+	{ languageOptions: { globals: globals.node } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	{
+		rules: {
+			"react/react-in-jsx-scope": "off",
+		},
+	},
 ];
