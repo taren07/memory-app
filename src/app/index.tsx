0bbd9e8c16c4c779e458e15e62c554e8fa457b74
taren07 <taro.enchi@gmail.com>
2025-01-09 00:58:26 +0900

    add header styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,55 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const Index = () => {
 	return (
-		<View>
-			<Text>Hello, world!</Text>
+		<View style={styles.container}>
+			<View>
+				<View>
+					<Text>Memory</Text>
+					<Text>ログアウト</Text>
+				</View>
+			</View>
+			<View>
+				<View>
+					<Text></Text>
+					<Text></Text>
+				</View>
+				<View>×</View>
+			</View>
+
+			<View>
+				<Text>+</Text>
+			</View>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+	},
+	header: {
+		backgroundColor: "#7dcddf",
+		height: 100,
+		justifyContent: "flex-end",
+	},
+	headerInner: {
+		alignItems: "center",
+	},
+	headerRight: {
+		position: "absolute",
+		right: 16,
+		bottom: 16,
+		color: "rgba(255, 255, 255, 0.8)",
+	},
+	headerTitle: {
+		marginBottom: 8,
+		fontSize: 24,
+		lineHeight: 32,
+		fontWeight: "bold",
+		color: "#fff",
+	},
+});
 
 export default Index;
